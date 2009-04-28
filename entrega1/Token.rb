701974f782f69246c2300b794d5df8c85ad56f7e
josejoeldejesus <josejoeldejesus@24002dc6-31c2-11de-8c0b-2fbac4a05959>
2009-04-28 21:35:08 +0000

    Basicamente inicie las clases Token y Lexer, cuando llegue a mi casa las termino, lexer basukasos es el lexer que has hecho hasta ahora subido al repositorio , Lexer es la clase para objetos a la que YO INTEGRARE TU CODIGO A LA CLASE COMO ACORDAMOS AYER CUANDO NOS REPARTIMOS EL TRABAJO, y la clase Token a la que me falta poco por agregar. Tu sube lo que hayas hecho.
    Las culminare hoy en la noche cuando llegue a mi casa.
    
    Saludillos sir.
    
    A    entrega1/Lexer.rb
    M    entrega1/Token.rb
    A    entrega1/lexer_bazukasos.rb

--- entrega1/Token.rb
+++ entrega1/Token.rb
@@ -1,17 +1,236 @@
+=begin
+@Tok={  "||"=>"TokOr", 
+        "&&"=>"TokAnd",
+        "+"=>"TokPlus",
+        "do"=>"TokDo",
+        "<="=>"TokLessEq",
+        "array"=>"TokArray",
+        "of"=>"TokOf",
+        ","=>"TokComma",
+        "-"=>"TokMinus",
+        "begin"=>"TokBegin",
+        "true"=>"TokTrue",
+        "out"=>"TokOut",
+        "fi"=>"TokFi",
+        "$"=>"TokLength",
+        "/"=>"TokDiv",
+        "proc"=>"TokProc",
+        ":"=>"TokTwoDots",
+        "%"=>"TokMod",
+        "in"=>"TokIn",
+        "["=>"TokOpBracet",
+        "end"=>"TokEnd",
+        "!="=>"TokNotEqual",
+        "<"=>"TokLess",
+        "->"=>"TokSelect",
+        "<-"=>"TokAssign",
+        "value"=>"TokValue",
+        "false"=>"TokFalse",
+        "="=>"TokEqual",
+        "show"=>"TokShow",
+        "as"=>"TokAs",
+        "if"=>"TokIf",
+        "]"=>"TokCloseBracket",
+        "main"=>"TokMain",
+        "var"=>"TokVar",
+        "~"=>"TokNot",
+        ">"=>"TokMore",
+        "("=>"TokOpenParen",
+        ">="=>"TokMoreEq",
+        "return"=>"TokReturn",
+        ")"=>"TokCloseParen",
+        "skip"=>"TokSkip",
+        "*"=>"TokMult",
+        "od"=>"TokOd",
+        ";"=>"TokDotComma"
+}
+=end
+
+
 class Token
     @col
     @line
     @value
     
-    def New (a=0, b=0, c=0)
-        unless (a.is_a?(Numeric) && b.is_a?(Numeric)) raise ArgumentError, "La fila y columna deben ser valores numericos."
+    attr_accessor :col, :line, :value
+    
+    def initialize (a=0, b=0, c=0)
+        unless (a.is_a?(Numeric) && b.is_a?(Numeric)) 
+            raise ArgumentError, "La fila y columna deben ser valores numericos."
+        end
         @col=a
         @line= b
         @value= c
     end
     
     def to_s
-        " Token "+ self.class + " Valor: "+@value+" Linea : "+@line+" Columna: "+@col
+        #" Token "+" Valor: "+ @value+" Linea : "+ @line+" Columna: "+@col
+        "Token " + self.class.to_s + " Valor: "+ @value.to_s + " Linea : " + @line.to_s + " Columna: " + @col.to_s  
     end
-    
 end
+
+ class TokDo < Token
+
+ end
+
+ class TokPlus < Token
+
+ end
+
+ class TokAnd < Token
+
+ end
+
+ class TokOr < Token
+
+ end
+
+ class TokComma < Token
+
+ end
+
+ class TokOf < Token
+
+ end
+
+ class TokLessEq < Token
+
+ end
+
+ class TokBegin < Token
+
+ end
+
+ class TokMinus < Token
+
+ end
+ class TokFi < Token
+
+ end
+ class TokOut < Token
+
+ end
+ class TokTrue < Token
+
+ end
+ class TokTwoDots < Token
+
+ end
+ class TokProc < Token
+
+ end
+ class TokDiv < Token
+
+ end
+ class TokLength < Token
+
+ end
+ class TokDotComma < Token
+
+ end
+ class TokEnd < Token
+
+ end
+ class TokOpBracet < Token
+
+ end
+ class TokIn < Token
+
+ end
+ class TokMod < Token
+
+ end
+ class TokValue < Token
+
+ end
+ class TokAssign < Token
+
+ end
+ class TokSelect < Token
+
+ end
+ 
+ class TokLess < Token
+
+ end
+ 
+ class TokNotEqual < Token
+
+ end
+
+ class TokVar < Token
+
+ end
+ 
+ class TokMain < Token
+
+ end
+
+ class TokCloseBracket < Token
+
+ end
+
+ class TokIf < Token
+
+ end
+
+ class TokAs < Token
+
+ end
+ 
+ class TokShow < Token
+
+ end
+ 
+ class TokEqual < Token
+
+ end
+
+ class TokFalse < Token
+
+ end
+ 
+ class TokArray < Token
+
+ end
+ 
+ class TokOpenParen < Token
+
+ end
+ 
+ class TokMore < Token
+
+ end
+
+ class TokNot < Token
+
+ end
+
+ class TokSkip < Token
+
+ end
+
+ class TokCloseParen < Token
+
+ end
+
+ class TokReturn < Token
+
+ end
+
+ class TokMoreEq < Token
+
+ end
+
+ class TokOd < Token
+
+ end
+
+ class TokMult < Token
+
+ end
+ 
+ 
+  x= TokMult.new(1,2,3)
+  
+  p x.to_s
