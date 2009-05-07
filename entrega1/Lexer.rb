0b8150012776aca86899ad898d238b2957d1bb32
Shaotic <Shaotic@24002dc6-31c2-11de-8c0b-2fbac4a05959>
2009-05-07 21:39:48 +0000

    conflictos resueltos.

--- entrega1/Lexer.rb
+++ entrega1/Lexer.rb
@@ -4,26 +4,33 @@ class Lexer
     attr_accessor :col, :line, :value
     
     def initialize archivo
-        @value = (File.open(archivo,'r')).readlines
+        @input = (File.open(archivo,'r'))
         @col=1
         @line=1
+	@buffer= @value.gets
     end
  
     def yylex
-		if (@line== nil ) then return nil end # Si es fin de archivo, chao pescado
-		a=value[line-1][col-1..value[line-1].size].strip
-		puts a
-		@@Exps.each { |x| if a.match(x) then b=@@Tok[x].new(col,line,$&);puts "#{b}";puts "#{$&.length}"; skip $&.length; return b end }
+	if (@buffer.lstrip! == nil ) then return nil end # Si es fin de archivo, chao pescado
+	
+	eliminar_comentarios()
+	puts @buffer
+	@@Exps.each { |x| if a.match(x) then b=@@Tok[x].new(col,line,$&);puts "#{b}";puts "#{$&.length}"; skip $&.length; return b end }
     end	
+
+	def eliminar_comentarios()
+		temp_line = ""
+	end
+
     
     def nl
-        @col=1
-        @line=@line+1
-		if (@line> @value.size ) then @line=nil end
+	@col=1
+        @line+=1
+	@buffer=@input.gets
     end
     
-    def skip n
+    def skip n=1
         @col+=n
-        if @col > @value[line-1][col-1..@value[line-1].size-2].size then nl end
+        if (@buffer.strip="/n" || @buffer.strip="") nl
     end
 end
