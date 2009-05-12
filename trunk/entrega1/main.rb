5f20af521dde597a3dc0c3de2d99d44c576d0972
Shaotic <Shaotic@24002dc6-31c2-11de-8c0b-2fbac4a05959>
2009-05-12 14:45:20 +0000

    modificado Lexer.rb y main.rb para mantener las reglas del enunciado 1 (particularmente lo que se refiere al archivo de lectura)

--- trunk/entrega1/main.rb
+++ trunk/entrega1/main.rb
@@ -6,10 +6,22 @@ require 'Lexer.rb'
 #
 #	Funcion principal que llama a la consola y a lexer para procesar bien archivos o bien comandos introducidos por el usuario
 #
-def start
-	lex= Lexer.new('lol.txt')
-	while ( (x= lex.yylex)!= nil )
+def start (archivo)
+  unless File.exists?(archivo)
+  throw Exception.new("El archivo \"#{prog_fn}\" no existe")
+  end
+  if File.directory?(archivo)
+  throw Exception.new("El archivo \"#{prog_fn}\" es un directorio")
+  end
+  unless File.readable_real?(archivo)
+  throw Exception.new("El archivo \"#{prog_fn}\" no se ha podido abrir para la lectura")
+  end
+  
+	lex= Lexer.new(File.open(archivo,'r'))
+	
+  while ( (tok= lex.yylex) != nil )
+  puts tok
 	end
 end
 
-start
+start "Ejemplos.txt"
