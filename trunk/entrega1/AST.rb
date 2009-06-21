99fd03f70f10401c31a9bcbdb172ee161e7d4d4b
josejoeldejesus@gmail.com <josejoeldejesus@gmail.com@24002dc6-31c2-11de-8c0b-2fbac4a05959>
2009-06-21 21:03:08 +0000

    El AST ya evalua expresiones aricméticas y lógicas. Aun falta bastante pero ahi vamos.

--- trunk/entrega1/AST.rb
+++ trunk/entrega1/AST.rb
@@ -1,4 +1,52 @@
 class AST
-   def initialize
+   @izq
+   @der
+   @op
+   @val
+   
+   attr_reader :izq, :der, :op, :val
+   
+   def initialize a, b, c
+      @izq=a
+      @der= b
+      @op= c
+   end
+   
+   def check
+   end
+   
+   def run
    end
 end
+
+class ASTMath < AST
+   
+   def check
+   end
+   
+   def run
+      @val=eval(@izq<<@op<<@der)
+   end
+end
+
+class ASTBool < AST
+
+   def check
+   end
+   
+   def run
+      @val=eval(@izq<<@op<<@der)
+   end
+end
+
+class ASTStmt < AST
+
+   def check
+   end
+   
+   def run
+   end
+end
+
+pr = ASTMath.new('3','17','<')
+p pr.run
